16aa7cf51d0a39403710dc69de0853b27aa91832
nishedh <restricted.karki@gmail.com>
2023-04-18 19:57:14 +0545

    added storage

--- scripts/content.js
+++ scripts/content.js
@@ -2,11 +2,13 @@ const main = () => {
   const button = document.querySelector('[aria-label="Claim Bonus"]');
   if (button) {
     button.click();
-    console.log(button);
     console.log("clicked!");
+    x = localStorage.getItem("clicked_n") || 0;
+    localStorage.setItem("clicked_n", x + 1);
+    console.log("current pickups: " + x + 1);
   } else {
     console.log("not found");
   }
 };
 
-setInterval(main, 30000); // every thirty seconds
+setInterval(main, 10000); // every thirty seconds
